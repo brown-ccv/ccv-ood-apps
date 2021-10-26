7c87571c67684cb5c0e0dc53acd102626b7b2321
ashokrags <ashok.ragavendran@gmail.com>
2021-10-26 18:15:12 -0400

    updated to latest working configs on server and local

---
+++ update_ood_apps.sh
@@ -0,0 +1,6 @@
+rsync -av bc_ccv_jupyter /var/www/ood/apps/sys/
+rsync -av bc_ccv_desktop /var/www/ood/apps/sys/
+rsync -av bc_ccv_desktop2 /var/www/ood/apps/sys/
+rsync -av bc_ccv_paraview /var/www/ood/apps/sys/
+rsync -av bc_ccv_rstudio /var/www/ood/apps/sys/
+rsync -av bc_ccv_tensorboard /var/www/ood/apps/sys/
