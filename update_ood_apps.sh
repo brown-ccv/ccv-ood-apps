a90778e449b1b3e4212d32055e01e74b38f6bd39
ashokrags <ashok.ragavendran@gmail.com>
2021-10-29 14:27:20 -0400

    Updated more configs

--- update_ood_apps.sh
+++ update_ood_apps.sh
@@ -4,3 +4,4 @@ rsync -av bc_ccv_desktop2 /var/www/ood/apps/sys/
 rsync -av bc_ccv_paraview /var/www/ood/apps/sys/
 rsync -av bc_ccv_rstudio /var/www/ood/apps/sys/
 rsync -av bc_ccv_tensorboard /var/www/ood/apps/sys/
+rsync -av bc_ccv_matlab /var/www/ood/apps/sys/
